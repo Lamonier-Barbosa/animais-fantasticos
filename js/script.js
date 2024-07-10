f9143a7d769455ff5e390c4d1d530442d0101cbb
Lamonier-Barbosa <lamoni_lr@hotmail.com>
2024-07-10 12:42:25 -0300

    divisao de arquivos js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimacaoScroll from "./modules/scroll-animacao.js";
+import initiAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tabnav.js";
+
+initScrollSuave();
+initAnimacaoScroll();
+initiAccordion();
+initTabNav();
